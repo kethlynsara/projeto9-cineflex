34c3f950a1fc47a7a9dadb32d81bfcf3ee73a514
Kethlyn <kethlynsaraa@gmail.com>
2022-03-24 17:01:51 -0300

    Buscar as informações dos filmes pela API fornecida e exibir conforme layout fornecido

--- src/componentes/Filmes/index.js
+++ src/componentes/Filmes/index.js
@@ -1,17 +1,35 @@
+import axios from "axios";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useState } from "react/cjs/react.development";
 
 import "../Filmes/style.css"
 
 function Filmes() {
 
-    const filmes = [<div className="filme"></div>, <div className="filme"></div>, <div className="filme"></div>, <div className="filme"></div>, <div className="filme"></div>, <div className="filme"></div>]
-    const filme = filmes.map(movie => movie);
+    const [filmes, setFilmes] = useState([])
+
+    useEffect(() => {
+        const promise = axios.get("https://mock-api.driven.com.br/api/v5/cineflex/movies");
+        promise.then((resposta) => {
+            const { data } = resposta;
+            setFilmes(data);
+        });
+    }, [])
+
     return (
         <>
             <h2 className="Filmes titulo">Selecione o filme</h2>
             <div className="Filmes colecao">
-                <Link to="/sessao"><div className="filme"></div></Link>
-                {filme}
+                {filmes.map(filme => {
+                    return <Link to="/sessao">
+                               <div className="filme-box">
+                                    <div className="movie"> 
+                                        <img src={filme.posterURL} alt={filme.title} />
+                                    </div>
+                                </div>
+                           </Link>
+                })}
             </div>
         </>
     )
